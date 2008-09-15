bcef4e95182f880e90b100d257052ac92a8304e4
neal <neal@tj>
2008-09-14 22:37:20 -0600

    add ChangeLog; general cleanup

--- makeauditunits.py
+++ makeauditunits.py
@@ -4,7 +4,7 @@
 %InsertOptionParserUsage%
 
 Example:
- makeauditunits.py cumulative-pe-s23-b005-m800.xml cumulative-pe-s24-b006-m803.xml cumulative-pe-s25-b007-m804.xml
+ makeauditunits cumulative-pe-s23-b005-m800.xml cumulative-pe-s24-b006-m803.xml cumulative-pe-s25-b007-m804.xml
 
 Use pydoc to produce full documentation.
 
@@ -12,8 +12,11 @@ Question:
  Where are ballot counts?  Total the ballots for Senate DEM+REP or something
 
 Todo:
+ Need a way to sort DRE results out separately
+ Name report for just one party, maybe check that there is just one
  Deal with namespace issues - auto-edit file, or handle it.
  Divide results into Early and Election, only output stuff that changes
+ Print "few" rather than a number less than 3?
  Option to either produce raw report, or combined to allow publication
  Encapsulate election-specific data in an Election class
    including replace_dict, fields of relevance, etc
@@ -26,20 +29,20 @@ Todo:
  Look for columns that don't agree with previous result
  Support for selecting sequence of contests in columns
  Produce totals
- Need a way to sort DRE results out separately
 
 Django implementation:
- Class Contest - model
+ class Contest - model
   margin
-  list of Results
- Class Result  based on GenericResult (dynamically created somehow, add choices)
+  # one-to-many with Results pointed to by them
+ class IncrementalResult
   contest
-  audit_unit
-  under, over
-  choice_a, choice_b....
- Class PublicView
+  early [under, over, choice_a, choice_b]
+  election [under, over, choice_a, choice_b]
+  generate_results
+
+ class PublicView
   contest
- Class RawView
+ class RawView
 
 http://code.djangoproject.com/wiki/DynamicModels plus syncdb and admin magic
 
@@ -54,10 +57,20 @@ import lxml.etree as ET
 from datetime import datetime
 
 __author__ = "Neal McBurnett <http://mcburnett.org/neal/>"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __date__ = "2008-09-08"
 __copyright__ = "Copyright (c) 2008 Neal McBurnett"
 __license__ = "GPL v3"
+
+class Result:
+    """     ?based on GenericResult (dynamically created somehow, add choices)?
+    def __init__()
+  contest
+  audit_unit
+  under, over
+  choice_a, choice_b....
+ Clever enumerating?  to return over, under, then each candidate
+ """
 
 replace_dict = {
     "REPRESENTATIVE TO THE 111th UNITED STATES CONGRESS - DISTRICT ": "CD",
@@ -70,7 +83,7 @@ replace_dict = {
 }
 
 
-parser = optparse.OptionParser(prog="makeauditunits.py", version=__version__)
+parser = optparse.OptionParser(prog="makeauditunits", version=__version__)
 
 parser.add_option("-v", "--verbose",
                   action="store_true", default=False,
@@ -95,16 +108,19 @@ def main(parser):
 
     logging.basicConfig(level=loglevel) # format='%(message)s'
 
+    logging.debug("args = %s" % args)
+
     totals = {}
     for file in args:
         newtotals = do_contests(file)
-        makeauditunit(totals, newtotals)
+        make_audit_unit(totals, newtotals)
         totals = newtotals
 
 def do_contests(file):
     """Extract relevant data from each contest in a given file"""
 
-    root = ET.parse("/tmp/cumulative-pe-s23-b005-m800.xml").getroot()
+    root = ET.parse(file).getroot()
+    logging.debug("root = %s" % root)
 
     values = {}
 
@@ -139,17 +155,34 @@ def do_contests(file):
 		                 'FormattedArea[@Type="Footer"]' ),
                                 fields )
         
-        # For each candidate or option
         v = []
         logging.debug(contesttree.getchildren())
+
+        # For each candidate or option
         for c in contesttree.xpath('.//FormattedAreaPair[@Type="Details"]'):
             cv = extract_values(c, fields )
             logging.debug("candidate: %s" % cv['Name'])
             v.append(cv)
 
-        parties = [cv['Party'] for cv in v]
-        tree_head.update({'Party': parties})
-        key = "%s:%s" % (contest, parties)
+        parties = set([cv['Party'] for cv in v])
+        assert len(parties) == 1
+        party = parties.pop()
+
+        tree_head.update({'Party': party})
+        key = "%s:%s" % (contest, party)
+
+        # TODO: if not there yet, put names into contest instance,
+        #  and establish order that the Results should be stored
+        # TODO: separate into two possible batches: Early and Election
+        # D2 = dict((key, D1[key]) for key in keys_you_want)
+        # or dict((k,v) for k,v in d.iteritems() if predicate(k))
+
+        candvotes_early = {}
+        candvotes_election = {}
+        for candidate in v:
+            candvotes_early[candidate['Name']] = candidate['Early/Absentee']
+            candvotes_election[candidate['Name']] = candidate['Election day']
+
         values[key] = [tree_foot, v]
 
         # For each candidate, get Election day and Early/Absentee
@@ -160,9 +193,11 @@ def do_contests(file):
     return values
 
 def extract_values(tree, fields):
-    """Extract values from given tree for fields"""
+    """Extract the values of any field listed in fields from given tree"""
 
-    """Return results as [ [headers...] [Early...]  [Election]]"""
+    # Return results as [ [headers...] [Early...]  [Election]] ?
+    # or in an array with column headers?
+    # nah - leave that all to Record....
 
     logging.debug("tree = %s, line %s" % (tree[0].tag, tree[0].sourceline))
     # "fields = %s" % fields
@@ -182,11 +217,44 @@ def extract_values(tree, fields):
     logging.debug("values = %s" % values)
     return values
 
-def makeauditunit(totals, newtotals):
+def make_audit_unit(totals, newtotals):
     """Subtract totals from newtotals and report the results for one audit unit"""
 
     import pprint
+
     pprint.pprint(newtotals)
+
+    for contest in newtotals:
+        print contest
+        for x in newtotals[contest]:
+            for f in x:
+                import pdb
+                #pdb.set_trace()	# put this where you want to start tracing
+                
+                #print("	%s	%s" % (f, x[f])) # - totals[contest][x][f])
+
 
 if __name__ == "__main__":
     main(parser)
+
+
+    """
+    python -m cProfile -s time makeauditunits.py testcum.xml 2>&1 > profile.out
+
+    or
+
+    import cProfile
+    cProfile.run('main(parser)')
+
+    or older:
+
+    import hotshot, hotshot.stats
+    prof = hotshot.Profile("test.prof")
+    #benchtime, stones = prof.runcall(test.pystone.pystones)
+    benchtime, stones = prof.runcall(main(parser))
+    prof.close()
+    stats = hotshot.stats.load("stones.prof")
+    stats.strip_dirs()
+    stats.sort_stats('time', 'calls')
+    stats.print_stats(20)
+    """
