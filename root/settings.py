bcfad81e4368cfe0a92bf83ec66ef8a661979ac3
Neal McBurnett <http://mcburnett.org/neal/>
2008-11-12 17:04:18 -0700

    cleaner mirroring: style.css => /media/style.css; trailing slash on /reports/1

--- root/settings.py
+++ root/settings.py
@@ -38,12 +38,12 @@ USE_I18N = True
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), "site_media")
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), "media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/site_media/'
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
